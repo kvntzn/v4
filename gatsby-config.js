86571ae9d8116853fd8a5d9acbf0aa5c6c7f232a
= <kevintuazon45@gmail.com>
2021-09-19 23:18:11 +0800

    update config and static resources

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Kevin Tuazon',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Kevin Tuazon is a software engineer who specializes in building (and occasionally designing) exceptional mobile apps.',
+    siteUrl: 'https://tuazonkevin.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@kvntzn',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'KevinTuazon',
+        short_name: 'KevinTuazon',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
