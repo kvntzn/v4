5dbc9a78352098c8627ee0472317663e48480946
Kevin Tuazon <kevintuazon45@gmail.com>
2023-09-09 10:30:34 +0800

    refactor: color changes

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,17 +2,39 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
-    --lightest-navy: #233554;
+    /** CSS DARK THEME PRIMARY COLORS */
+    --color-primary-100: #382bf0;
+    --color-primary-200: #5e43f3;
+    --color-primary-300: #7a5af5;
+    --color-primary-400: #9171f8;
+    --color-primary-500: #a688fa;
+    --color-primary-600: #ba9ffb;
+    /** CSS DARK THEME SURFACE COLORS */
+    --color-surface-100: #121212;
+    --color-surface-200: #282828;
+    --color-surface-300: #3f3f3f;
+    --color-surface-400: #575757;
+    --color-surface-500: #717171;
+    --color-surface-600: #8b8b8b;
+    /** CSS DARK THEME MIXED SURFACE COLORS */
+    --color-surface-mixed-100: #1a1625;
+    --color-surface-mixed-200: #2f2b3a;
+    --color-surface-mixed-300: #46424f;
+    --color-surface-mixed-400: #5e5a66;
+    --color-surface-mixed-500: #76737e;
+    --color-surface-mixed-600: #908d96;
+
+    --dark-navy: var(--color-surface-100);
+    --navy: var(--color-surface-mixed-100);
+    --light-navy: var(--color-surface-mixed-200);
+    --lightest-navy: var(--color-surface-mixed-300);
     --navy-shadow: rgba(2, 12, 27, 0.7);
     --dark-slate: #495670;
-    --slate: #8892b0;
-    --light-slate: #a8b2d1;
-    --lightest-slate: #ccd6f6;
+    --slate: #8b8b8b;
+    --light-slate: #8b8b8b;
+    --lightest-slate: #fff;
     --white: #e6f1ff;
-    --green: #64ffda;
+    --green: var(--color-primary-500);
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
